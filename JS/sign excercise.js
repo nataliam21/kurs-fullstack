ef9e3ac0f5e27bc04e94bce92dd9bf7fc5a95cbe
natalia_m <n.meisnerowska@gmail.com>
2023-08-10 17:37:23 +0200

    exercise 2

--- JS/sign excercise.js
+++ JS/sign excercise.js
@@ -4,3 +4,9 @@ console.log(str[0]);
 console.log(str[str.length - 1]);
 let txt = str + "🚀";
 console.log(txt);
+
+let pet1 = "kota";
+let pet2 = "psa";
+
+let str1 = `Ala ma ${pet2}, a Ola ma ${pet1}`;
+console.log(str1);
