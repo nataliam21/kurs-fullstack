a3727dd55241bafdf27797c8645672537b1caef7
natalia_m <n.meisnerowska@gmail.com>
2023-08-10 17:24:47 +0200

    sign excercise

---
+++ JS/sign excercise.js
@@ -0,0 +1,6 @@
+let str = "Życie jak w Madrycie!";
+console.log(str);
+console.log(str[0]);
+console.log(str[str.length - 1]);
+let txt = str + "🚀";
+console.log(txt);
---
+++ JS/string.js
@@ -0,0 +1,18 @@
+/* string to prosty typ danych będący łańcuchem znaków zwany również literałem łańcuchowym lub literałem znakowym */
+
+let str = "Hello World";
+let str2 = "Hello";
+
+let msg = str2 + " World!";
+console.log(msg);
+
+let strLen = msg.length;
+console.log(strLen);
+
+let firstSign = msg[0];
+console.log(firstSign);
+console.log(msg[11]);
+console.log(msg[4]);
+
+let lastSing = msg[msg.length - 1];
+console.log(lastSing);
